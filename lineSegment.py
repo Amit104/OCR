af6e77ca227fd3ff9ba7ed1a6951319b18250fca
Amit104 <wadatkar.amit104@gmail.com>
2017-01-22 01:35:54 +0530

    Line Segment almost done

--- lineSegment.py
+++ lineSegment.py
@@ -8,15 +8,16 @@ Created on Sat Jan 21 19:17:25 2017
 from initialisations import *
 
 def getNodeNumber(r, c, x, y):
-    return (x * c) + y + 1
+    return int((x * c) + y + 1)
         
 diction = {}
 pathList = []
 
             
 def startBFSandReturnPathList(IM, r, c, brkPts, size):
-    for i in range(1, r * c):
-        diction[i] = [-1 for x in range(1, 4)]
+    for i in range(r):
+        for j in range(c):
+            diction[getNodeNumber(r, c, i, j)] = [i, j, -1, -1]
     
     for i in range(1, size):
         bfs(IM, getNodeNumber(r, c, (brkPts[i - 1] + brkPts[i]) / 2 , 0), brkPts[i - 1], brkPts[i], r, c)
@@ -25,8 +26,46 @@ def startBFSandReturnPathList(IM, r, c, brkPts, size):
     
 def LineSeg(IM, r, c, brkPts, size):
     startBFSandReturnPathList(IM, r, c, brkPts, size)
+    #first row
     
-    
+    for i in range(1,size-1):
+        #above
+        currPath = pathList[i]
+        for j in currPath:
+            x = diction[j][0]
+            y = diction[j][1]
+            print(IM[x][y])
+        prevPath = pathList[i-1]
+        mini = prevPath[len(prevPath) - 2]
+        maxi = currPath[len(currPath) - 1]
+        
+        #initialise temporary image
+        imag = []  
+        for j in range(maxi-mini+1):                               
+            il = []         
+            for k in range (c):            
+                il.append(0)      
+            imag.append(il) 
+            
+        for j in prevPath[:-2]:
+            for k in range(mini,diction[j][0]+1):
+                imag[k-mini][diction[j][1]] = 255
+            
+        for j in currPath[:-2]:
+            for k in range(diction[j][0],maxi+1):
+                imag[k-mini][diction[j][1]] = 255
+        
+        for j in range (maxi-mini+1):                                
+            for k in range (c):     
+                if imag[j][k]==0:
+                    imag[j][k]=IM[j+mini][k] 
+           
+        imag = np.array(imag,dtype=np.uint8)
+        
+        cv2.imshow('image',imag)
+        cv2.waitKey(0)  
+        cv2.destroyAllWindows()
+    #last row
 
 def bfs(IM, start, x1, x2, r, c):
     x = diction[start][0]
@@ -42,42 +81,54 @@ def bfs(IM, start, x1, x2, r, c):
         
         #right node
         if x + 1 <= x2 and IM[x + 1][y] == 255:
-            neighbour = getNodeNumber(x + 1, y)
-            if diction[neighbour][3] == 0:
+            neighbour = getNodeNumber(r,c,x + 1, y)
+            if diction[neighbour][3] == -1:
                 diction[neighbour][3] = 1
                 diction[neighbour][2] = current
                 vertQueue.enqueue(neighbour)
         
         #left node
         if x - 1 >= x1 and IM[x - 1][y] == 255:
-            neighbour = getNodeNumber(x - 1, y)
-            if diction[neighbour][3] == 0:
-                diction[neighbour][3] = 1
-                diction[neighbour][2] = current
-                vertQueue.enqueue(neighbour)
-                
-        #top node
-        if y - 1 >= 0 and IM[x][y - 1] == 255:
-            neighbour = getNodeNumber(x, y - 1)
-            if diction[neighbour][3] == 0:
+            neighbour = getNodeNumber(r,c,x - 1, y)
+            if diction[neighbour][3] == -1:
                 diction[neighbour][3] = 1
                 diction[neighbour][2] = current
                 vertQueue.enqueue(neighbour)
                 
         #bottom node
         if y + 1 <= c - 1 and IM[x][y + 1] == 255:
-            neighbour = getNodeNumber(x, y + 1)
-            if diction[neighbour][3] == 0:
+            neighbour = getNodeNumber(r,c,x, y + 1)
+            if diction[neighbour][3] == -1:
                 diction[neighbour][3] = 1
                 diction[neighbour][2] = current
                 vertQueue.enqueue(neighbour)
         
+        #top node
+        if y - 1 >= 0 and IM[x][y - 1] == 255:
+            neighbour = getNodeNumber(r,c,x, y - 1)
+            if diction[neighbour][3] == -1:
+                diction[neighbour][3] = 1
+                diction[neighbour][2] = current
+                vertQueue.enqueue(neighbour)
 
 def getPath(NodeNumber):
     path = []
+    minP = 1000000
+    maxP = -1
     while not diction[NodeNumber][2] == -1:
+        x = diction[NodeNumber][0]
+        if minP > x:
+            minP = x
+        if maxP < x:
+            maxP = x
         path.append(NodeNumber)
         NodeNumber = diction[NodeNumber][2]
+    x = diction[NodeNumber][0]
+    if minP > x:
+        minP = x
+    if maxP < x:
+        maxP = x
     path.append(NodeNumber)
+    path.append(minP)
+    path.append(maxP)
     return path
-    
--- ml.py
+++ ml.py
@@ -9,34 +9,30 @@ from initialisations import *
 from denoise import *
 from lineSegment import *
 
-def getSum(r,c,IM):
-    sum1 = []
-    for i in range(r):
-        rowsum = 0
-        for j in range(c):
-            if IM[i][j] == 0:
-                rowsum = rowsum + 1
-        sum1.append(rowsum)
-        
-        
-def showImage(x):
-    cv2.imshow('image',x)
-    cv2.waitKey(0)
+def showImg(th2):
+    cv2.imshow('image',th2)
+    cv2.waitKey(0)  
     cv2.destroyAllWindows()
-
-
+    
 img = cv2.imread('D:/Study/ML/OCR/hh2.jpg',0)
 cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-ret2,IM = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-
-r = IM.shape[0]
-c = IM.shape[1]
-
-sum1 = getSum(r,c,IM)
-f = interp1d(range(r), sum1)
-
-x = range(r)
-y = sum1
-
-plt.plot(range(r), sum1,'o', range(r), f(range(r)), '-',)
+ret2,th2 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+r = th2.shape[0]
+c = th2.shape[1]
+avg = []
+for i in range(r):
+    rowsum = 0
+    f= 0
+    for j in range(c):
+        if th2[i][j] == 0:
+            rowsum = rowsum + 1
+            f = 1
+    avg.append(rowsum)
+f = interp1d(range(r), avg)
+plt.plot(range(r), avg,'o', range(r), f(range(r)), '-',)
 plt.show()
+
+brkpts = [11,30,50,70,90,109,129,150,168,188,208,226]
+brkpts2 = [115,357,532,677,834,1022,1221,1385,1590,1827,2056,2219,2389,2570,2782]
+brkpts3 = [105,285,495,671]
+LineSeg(th2,r,c,brkpts2,15)
